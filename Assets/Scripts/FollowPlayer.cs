4e6ad765a0eb0f74acbd07f49dc18c55454ac64d
John D. Aynedjian <john.david.aynedjian@gmail.com>
2020-09-11 00:25:40 -0400

    1.3.2-new-script-for-the-camera --Camera follows player

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"FollowPlayer::constructor(): this.player = {this.player}...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Assign camera's position to player's position...
+        this.transform.position = this.player.transform.position;
+    }
+}

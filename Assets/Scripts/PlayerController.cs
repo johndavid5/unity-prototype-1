2abfb0b1decae35ab5d89dded095cbb0f91e9d16
John D. Aynedjian <johndavid5@yahoo.com>
2020-09-15 22:42:44 -0400

    1.4.2--base-turn-speed-on-input (#10)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 20.0f; // meters/second
-    public float turnSpeed; // meters/second
+    public float turnSpeed = 10.0f; 
+    public float horizontalInput;
 
     // Arrow Function Format of ToString()...
     // public override string ToString() => GetType().FullName;
@@ -32,16 +33,6 @@ public class PlayerController : MonoBehaviour
     * @see https://docs.unity3d.com/ScriptReference/Debug.Log.html
     */
     PlayerController() {
-
-        // Message with a GameObject name...
-        // UnityException: get_gameObject is not allowed to be called from a MonoBehaviour constructor(or instance field initializer), call it in Awake or Start instead. Called from MonoBehaviour 'PlayerController' on game object 'Veh_Ute_Red_Z'.
-        // See "Script Serialization" page in the Unity Manual for further details.
-        // PlayerController..ctor()(at Assets / Scripts / PlayerController.cs:37)
-        // Debug.Log($"Hello, {this.gameObject.name}!");
-
-        // NOTE: Console.WriteLine() doesn't appear in the Unity console...
-        // Console.WriteLine($"The {ToString()} constructor is executing.");
-
         this.count = 0;
 
         // Debug.Log("PlayerController::PlayerController() -- constructor -- I'll be back, Bennett!");
@@ -63,15 +54,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame...50-60 times per second...1/50-1/60 seconds per frame...0.02-0.01666 seconds per frame...
     void Update()
     {
+        // Procure horizontalInput from Input manager...see Horizontal in Edit | Project Settings | Input 
+        this.horizontalInput = Input.GetAxis("Horizontal");
+
         // Move the vehicle forward...
         this.count++;
         this.transform.Translate(Vector3.forward * Time.deltaTime * this.speed );
-        this.transform.Translate(Vector3.right * Time.deltaTime * this.turnSpeed);
+        this.transform.Translate(Vector3.right * Time.deltaTime * this.turnSpeed * this.horizontalInput);
 
         // 1 meter * 1 / 50 seconds per frame = 1/50 meter/frame; 1/50 meters/frame * 50 frames/second = 1 meter/second
         Debug.Log($"{ToString(MethodBase.GetCurrentMethod())}: {this.count}: Time.time = {Time.time}:\n" +
-            "this.turnSpeed = {this.turnSpeed}, Time.deltaTime = {Time.deltaTime},\n" +
-            "this.transform.position = {this.transform.position}..." );
+            $"this.turnSpeed * this.horizontalInput = {this.turnSpeed * this.horizontalInput}\n" +
+            $"Time.deltaTime = {Time.deltaTime},\n" +
+            $"this.transform.position = {this.transform.position}..." );
     }
 
 }
